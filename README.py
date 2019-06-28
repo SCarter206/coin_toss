6329c05990ac0da2226b631fd3e36d54f6051241
Sean Carter <48145510+SCarter206@users.noreply.github.com>
2019-06-28 00:34:57 -0700

    Update README.py
    learning to use classes and when. clearly overkill for a simple program

--- README.py
+++ README.py
@@ -1,59 +1,71 @@
 import random
-#improved argument positions, clearer notes, initialized variables(bank, y), user_bank wont add or subtract just provides uneffected bank value
+bank = 100
+#reminds user of choice they made, 0heads or 1tails
 class coin_proccess():
-    bank = 0
-    y = 0
-#recieves user input, heads or tails using 0 and 1
-    def user_coin_choice(x):
-        y = eval(input("0 for heads and 1 for tails\n"))
-        if y == 0:
+    def user_coin_choice(recieve_user_coin_input):
+        if recieve_user_coin_input == 0:
             print("you chose heads")
-            return 'heads'
-        elif y == 1:
+            return "heads"
+        elif recieve_user_coin_input == 1:
             print("you chose tails")
-            return 'tails'
+            return "tails"
         else:
             return "just type 0 or 1 and press enter"
-#recieves amount user wants to bet
-    def user_bet(y):
-        y = eval(input("how much would you like to bet\n"))
-        return y
-#computer randomly selects heads or tails and notifies user
-    def coin_flip(z):
+#computer chooses 0heads or 1tails at random
+class comp_coin_flip():
+    def coin_flip(list):
         list = [0, 1]
-        z = random.choice(list)
-        if z == 0:
-            print("coin has landed on heads")
-            return 'heads'
-        elif z == 1:
-            print("coin has landed on tails")
-            return 'tails'
-#compares users input and comp. random choice
-    def compare(coin_flip, user_coin_choice):
-        if coin_flip == user_coin_choice:
+        random.choice(list)
+        if x == 0:
+            return("heads")
+        elif x == 1:
+            return("tails")
+#asks and stores users input 0heads or 1tails
+class recieve_user_coin_input():
+        def user_coin_input(y):
+            y = eval(input("0 for heads and 1 for tails\n"))
+            return y
+#asks and stores users bet
+class recieve_user_bet_input():
+    def get_user_bet(z):
+        z = eval(input("how much would you like to bet\n"))
+        return z
+#compares users coin choice and computers coin choice
+class compare_coin_results():
+    def compare(comp_coin_flip, recieve_user_coin_input):
+        if comp_coin_flip == recieve_user_coin_input:
             return "you won"
         else:
             return "sorry try again"
-#takes user bet amount provided earlier and adds or subtracts from bank
-    def user_bank(compare, bank, y):
-        bank = 100
-        y = int()
-        if compare == "you won":
-            print("you won and now have a score of " + str(bank + y))
-
+#adds to or subtracts from bet amount and stores for next round
+class bank_effects():
+    bank = 100
+    def user_bank(compare_coin_results, bank):
+        if compare_coin_results() == "you won":
+            return "you now have " + bank + z
         else:
-            print("you lost and now have a score of " + str(bank - y))
-
+            return "you now have " + bank - z
 
 def main():
+    print("Welcome to heads or tails. You'll start your bank with $100")
 
-    print("Welcome to heads or tails. You will start with $100")
+    obj = recieve_user_coin_input()
+    obj.user_coin_input()
+
+    obj = recieve_user_bet_input()
+    obj.get_user_bet()
+
     obj = coin_proccess()
     obj.user_coin_choice()
-    obj.user_bet()
-    obj.coin_flip()
-    obj.compare('user_coin_choice')
-    obj.user_bank('compare', 'y')
+
+    obj = comp_coin_flip()
+    obj.coin_flip(list)
+
+    obj = compare_coin_results()
+    obj.compare(comp_coin_flip, recieve_user_coin_input)
+
+    obj = bank_effects()
+    obj.user_bank(compare_coin_resluts, bank)
 
 if __name__ == "__main__":
-        main()
+    main()
