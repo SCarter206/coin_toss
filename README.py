42a6d73ac506a185d4d4fa20ba6929f1e2982384
Sean Carter <seans206email@gmail.com>
2019-07-02 21:37:47 -0700

    Update README.py
    progress made so far. adding a subtracting not working. will need to store new number for game to continue until user_bank =0

--- README.py
+++ README.py
@@ -1,74 +1,73 @@
-# coin_toss
-#heads or tails project in python 3
-#im getting a positional error code:
-"""
-Welcome to heads or tails. You'll start your bank with $100
-0 for heads and 1 for tails
-1
-you chose tails
-how much would you like to bet
-1
-Traceback (most recent call last):
-  File "coin_toss.py", line 53, in <module>
-    main()
-  File "coin_toss.py", line 48, in main
-    obj.compare()
-TypeError: compare() missing 1 required positional argument: 'user_coin_choice'
-"""
-# ive google searched the rules on positional arguments but im not finding literature regarding the whole file. Any advice appreciated.
-# just a beginner
+
+
+1 of 8
+(no subject)
+
+sean carter <seans206email@gmail.com>
+Attachments
+Thu, Jun 27, 11:52 PM (5 days ago)
+to brittany.r.jones
+
+
+Attachments area
 
 import random
-bank = 100
 
 class coin_proccess():
-    def user_coin_choice(y):
+    bank = 0
+    y = 0
+#recieves user input, heads or tails using 0 and 1
+    def user_coin_choice(x):
         y = eval(input("0 for heads and 1 for tails\n"))
         if y == 0:
             print("you chose heads")
-            return "heads"
+            return 'heads'
         elif y == 1:
             print("you chose tails")
-            return "tails"
+            return 'tails'
         else:
             return "just type 0 or 1 and press enter"
-
-    def get_user_bet(z):
-        z = eval(input("how much would you like to bet\n"))
-        return z
-
-    def coin_flip(list, x):
+#recieves amount user wants to bet
+    def user_bet(y):
+        y = eval(input("how much would you like to bet\n"))
+        return y
+#computer randomly selects heads or tails and notifies user
+    def coin_flip(z):
         list = [0, 1]
-        x = random.choice(list)
-        if x == 0:
-            return("heads")
-        elif x == 1:
-            return("tails")
-
+        z = random.choice(list)
+        if z == 0:
+            print("coin has landed on heads")
+            return 'heads'
+        elif z == 1:
+            print("coin has landed on tails")
+            return 'tails'
+#compares users input and comp. random choice
     def compare(coin_flip, user_coin_choice):
-        if coin_flip() == user_coin_choice():
+        if coin_flip == user_coin_choice:
             return "you won"
         else:
             return "sorry try again"
+#takes user bet amount provided earlier and adds or subtracts from bank
+#currently does not add or subtracts. This new number will need to be stored for game to continue until user_bank = 0    
+    def user_bank(compare, bank, y):
+        bank = 100
+        y = int()
+        if compare == "you won":
+            print("you won and now have a score of " + str(bank + y))
 
-    def user_bank(compare):
-        if compare() == "you won":
-            return "you now have " + bank + z
         else:
-            return "you now have " + bank - z
+            print("you lost and now have a score of " + str(bank - y))
 
 
 def main():
-    print("Welcome to heads or tails. You'll start your bank with $100")
 
+    print("Welcome to heads or tails. You will start with $100")
     obj = coin_proccess()
     obj.user_coin_choice()
-    obj.get_user_bet()
-    obj.coin_flip('x')
-    obj.compare()
-    obj.user_bank()
-
+    obj.user_bet()
+    obj.coin_flip()
+    obj.compare('user_coin_choice')
+    obj.user_bank('compare', 'y')
 
 if __name__ == "__main__":
-    main()
-
+        main()
